9e34e450efe961a51e61a62fa4363eeb49f18f8e
Maicol <michael.negrete16@gmail.com>
2022-09-09 19:21:03 -0500

    Refactorizacion de codigo mas Diseño 2.0

--- src/CRM/page/CrmPage.jsx
+++ src/CRM/page/CrmPage.jsx
@@ -11,85 +11,44 @@ const CrmPage = () => {
     const openModal = () => {
         openCrmModal()
     }
-
+    
     const handleClickNew = () => {
 
     }
 
     return (
-        // <div className='container'>
-        //     <FormRegistro/>
-        //     <FormResultado/>
-        //     <CrmModal/>
-        // </div>
-
         <div >
-            {/* <nav className='menu' tabIndex='0'>
-            <div className='smartphone-menu-trigger'></div>
-                <header className='avatar'>
-                    <img src="https://cdn.icon-icons.com/icons2/1805/PNG/512/4230530-market-market-store-shop-store_115032.png" alt="IconoMercado" />
-                    <h2>MINI - CRM</h2>
-                </header>
 
-                <ul>
+            <div className='marcoPrincipal'>
+                <div className='marcoSecundario'>
+                    <div className='principalSearch'>
+                            <input type="search"  placeholder='Buscar'/>
+                            <i className="fa-solid fa-magnifying-glass"></i>
+                    </div>
 
-                    <li tabIndex='0' className='icon-add' >
-                        <button onClick={openModal}>
-                            <span>Add New</span>
+                    <div>
+                        <FormResultado/>
+                    </div>
+
+                    <div>
+                        <button onClick={openModal} className='btn'> Agregar
+                            <span> </span>
+                            <i className="fa-solid fa-user-plus"></i>
                         </button>
-                    </li>
-
-                    <li tabIndex='0' className='icon-search'>
-                        <a href="https://www.linkedin.com/in/michael-negrete-0a3938206/" target='blank'>
-                            <span>Linkedin</span>
-                        </a>
-                    </li>
-
-                    <li tabIndex='0' className='icon-search'>
-                        <a href="https://github.com/MichaelNegrete16" target='blank'>
-                            <span>GitHub</span>
-                        </a>
-                    </li>
-
-                </ul>
-
-            </nav>
-
-            <main>
-                <dir className='helper'>
-                    <FormResultado/>
-                </dir>
-            </main>
-
-
-            <CrmModal/> */}
-
-            <div>
-                <div className='principalSearch'>
-                        <input type="search"  placeholder='Buscar'/>
-                        <i className="fa-solid fa-magnifying-glass"></i>
-                </div>
-
-                <div>
-                    <FormResultado/>
-                </div>
-
-                <div>
-                    <button onClick={openModal} className='btn'> Agregar
-                        <span> </span>
-                        <i className="fa-solid fa-user-plus"></i>
-                    </button>
-                </div>
-
-                <div className='footer'>
-                    <div className='footerContent'>
-                        <button><i className="fa-solid fa-address-book"></i></button>
                     </div>
-                    <div className='footerLink'>
-                        <a><i className="fa-brands fa-linkedin"></i></a>
-                    </div>
-                    <div className='footerGithub'>
-                        <a><i className="fa-brands fa-github"></i></a>
+
+                    <div className='marcoFooter'>
+                        <div className='footer'>
+                            <div className='footerContent'>
+                                <button><i className="fa-solid fa-address-book"></i></button>
+                            </div>
+                            <div className='footerLink'>
+                                <a><i className="fa-brands fa-linkedin"></i></a>
+                            </div>
+                            <div className='footerGithub'>
+                                <a><i className="fa-brands fa-github"></i></a>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
